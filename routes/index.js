d3116e7e7952bbc218a16499130238fd41123e7a
Gucal <gucalaliriza@outlook.com>
2019-10-17 15:14:57 +0300

    boostrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sanayim - 81 İl, Yüzlerce Yatırımcı, Binlerce Üretici' });
 });
 
 module.exports = router;
