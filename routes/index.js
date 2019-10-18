9c7773258099c742b8c49658f52aa6fa30addeb3
Gucal <gucalaliriza@outlook.com>
2019-10-19 01:50:27 +0300

    bootstrap changed

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sanayim - 81 İl, Yüzlerce Yatırımcı, Binlerce Üretici' });
+
 });
 
 module.exports = router;
