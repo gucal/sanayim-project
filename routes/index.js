fab4089ce7d516341852920eb549ae2b9421963e
Gucal <gucalaliriza@outlook.com>
2019-10-19 03:16:56 +0300

    hbs plan completed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sanayim - 81 İl, Yüzlerce Yatırımcı, Binlerce Üretici' });
-
 });
 
 module.exports = router;
