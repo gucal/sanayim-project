aaedf5875ce83bb1ced9d1e750099a815d9d2beb
Gucal <gucalaliriza@outlook.com>
2019-12-06 23:43:41 +0300

    add to addProduct page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var addProductRouter = require('./routes/addProduct');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/giris', loginRouter);
+app.use('/urun', addProductRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
