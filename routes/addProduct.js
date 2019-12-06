8a71d34d0b93a29f11227e0fff98038fd5d78a0e
Gucal <gucalaliriza@outlook.com>
2019-12-07 00:40:23 +0300

    edit title's

--- routes/addProduct.js
+++ routes/addProduct.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('addProduct');
+  res.render('addProduct',{ title:'Sanayim\'de ürün sat' });
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login',{title:'Login'});
+  res.render('login',{ title: 'Sanayim üye girişi' });
 });
 
 module.exports = router;
