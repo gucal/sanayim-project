7454ced16c4f535682647ec9fc422a96b57dd1e4
Gucal <gucalaliriza@outlook.com>
2019-12-09 15:38:10 +0300

    db'ye veri aktarımı tamamlanmıştır.

--- connectdb.js
+++ connectdb.js
@@ -7,8 +7,18 @@ let connection = mysql.createConnection({
     database: 'sanayim'
 })
 
-connection.connect(function (err) {
-    if(err) throw err;
-    console.log('Veri tabanı bağlantısı başarıyla gerçekleşti.');
+/* ID BOLUMU SUREKLI OLARAK *NULL* KALMALIDIR */
+// let sqlSorgusu = `INSERT INTO kullanicilar VALUES('8','abdurrahimbalta@outlook.com','12613','Abdurrahim BALTA','BMW','Otomobil','İstanbul');`
 
+
+connection.connect(function (err) {
+    if (err) throw err;
+    connection.query(sqlSorgusu, function (err, result) {
+        if (err) throw err.message;
+        console.log('Kulllanıcı başarılı bir şekilde eklendi');
+    });
+    connection.end(function (err) {
+        if (err) throw err;
+        console.log('Bağlantı sona erdi.');
+    })
 });
