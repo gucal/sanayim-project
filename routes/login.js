071e3290fe2dda7400dea128bf6ae89c85a6d36b
Gucal <gucalaliriza@outlook.com>
2019-12-11 03:30:12 +0300

    edit login.js

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-var mysql = require('mysql');
-
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'sanayim'
-})
-
-connection.connect(function(err){
-  if (err) throw err;
-  console.log('DB ON');
-})
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login',{ title: 'Sanayim üye girişi' });
 });
-
-app.post('/submitForm',function(req,res){
-  let sqlSorgusu = `INSERT INTO kullanicilar VALUES(NULL,'abdurrahimbalta@outlook.com','12613','Abdurrahim BALTA','BMW','Otomobil','İstanbul');`
-  connection.query(sqlSorgusu,function(err){
-    if (err) throw err;
-    console.log('BAŞARILI BİR ŞEKİLDE KULLANICI EKLENDİ');
-  })
-})
 
 module.exports = router;
