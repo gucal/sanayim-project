1b30a7571f6b23bebaa0f0ccb1b1dedffe84db4e
Gucal <gucalaliriza@outlook.com>
2019-12-13 01:51:36 +0300

    delete file

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('jspages'));
 
 app.use('/', indexRouter);
 app.use('/giris', loginRouter);
