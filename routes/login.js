be7da4db918254b0465f982460c69a2380c4a0af
Gucal <gucalaliriza@outlook.com>
2019-12-15 14:44:01 +0300

    edit

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login',{ title: 'Sanayim üye girişi' });
+router.get('/', function (req, res, next) {
+  res.render('login', { title: 'Sanayim üye girişi' });
 });
 
 module.exports = router;
