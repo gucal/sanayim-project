5f6d27f5d23f72d1135cc4441e841817e7ba2a43
Gucal <gucalaliriza@outlook.com>
2019-12-31 02:54:24 +0300

    latest commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var urunVerileri = require('./urunVerileri');
+var dbconfig = require('../database');
+var mysql = require('mysql');
+var connection = mysql.createConnection(dbconfig.connection);
 
 
 
+router.get('/detail/:id', function (req, res) {
+  var veriCekmeSorgusu = connection.query('SELECT * FROM urunlistesi  HAVING id=' + req.params.id);
+  veriCekmeSorgusu.on('result', function (row) {
+    var urunID = row['id'];
+    var urunAdi = row['urunAdi'];
+    var urunFiyati = row['urunFiyat'];
+    var kategoriAdi = row['kategoriAdi'];
+    var uretimDurumu = row['uretimDurumu'];
+    var urunDetaylari = row['urunDetaylari'];
+    var stokSayisi = row['stokSayisi'];
+    var sehirAdi = row['sehirSayisi'];
+    var urunResmi = row['urunResmi'];
+    if (uretimDurumu == 1) {
+      uretimDurumu = "Satışa hazır.";
+    } else { uretimDurumu = "İstek doğrultusunda üretilir."; }
+    res.render('details', { title: 'Detaylar', urunID, urunAdi, urunFiyati, kategoriAdi, uretimDurumu, urunDetaylari, stokSayisi, sehirAdi, urunResmi })
+  })
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,5 +34,6 @@ router.get('/', function (req, res, next) {
   });
   console.log(urunVerileri.urunAdi);
 });
+
 
 module.exports = router;
